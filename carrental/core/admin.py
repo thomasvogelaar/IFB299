8c6c7d623ec05051efacc720453626d5c930e4cf
Thomas Vogelaar <thomas@vogelaar.com.au>
2018-09-02 21:20:01 +1000

    Added car, customer, store and transaction forms to the admin panel.

--- carrental/core/admin.py
+++ carrental/core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Car, Customer, Store, Transaction
 
+admin.site.register(Car)
+admin.site.register(Store)
+admin.site.register(Customer)
+admin.site.register(Transaction)
 # Register your models here.
