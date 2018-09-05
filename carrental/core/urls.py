4e02cad65167d77d86a7b04de04a4bbe4eeb70f1
Thomas Vogelaar <thomas@vogelaar.com.au>
2018-09-05 13:26:53 +1000

    Added blank pages for car, customer and transaction lists and views.

--- carrental/core/urls.py
+++ carrental/core/urls.py
@@ -5,4 +5,11 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('stores', views.storelist, name='storelist'),
+    path('stores/<int:store_id>', views.storedetails, name='storedetails'),
+    path('cars', views.carlist, name='carlist'),
+    path('cars/<int:car_id>', views.cardetails, name='cardetails'),
+    path('customers', views.customerlist, name='customerlist'),
+    path('customers/<int:customer_id>', views.customerdetails, name='customerdetails'),
+    path('transactions', views.transactionlist, name='transactionlist'),
+    path('transactions/<int:transaction_id>', views.transactiondetails, name='transactiondetails')
 ]
--- carrental/core/views.py
+++ carrental/core/views.py
@@ -10,3 +10,38 @@ def index(request):
 @login_required
 def storelist(request):
     return HttpResponse("This is the list of stores")
+
+
+@login_required
+def storedetails(request, store_id):
+    return HttpResponse("This is the details page for store id %s." % store_id)
+
+
+@login_required
+def carlist(request):
+    return HttpResponse("This is the list of cars")
+
+
+@login_required
+def cardetails(request, car_id):
+    return HttpResponse("This is the details page for car id %s." % car_id)
+
+
+@login_required
+def customerlist(request):
+    return HttpResponse("This is the list of customers")
+
+
+@login_required
+def customerdetails(request, customer_id):
+    return HttpResponse("This is the details page for customer id %s." % customer_id)
+
+
+@login_required
+def transactionlist(request):
+    return HttpResponse("This is the list of transactions")
+
+
+@login_required
+def transactiondetails(request, transaction_id):
+    return HttpResponse("This is the details page for transaction id %s." % transaction_id)
