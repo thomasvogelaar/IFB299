97c12a4f47930b0f075cb3c94e6d5b8f3ffb151c
Logan Otto <log224@hotmail.com>
2018-09-30 15:19:01 +1000

    Refactored code, moving non-view function into a helper file.

--- carrental/core/views.py
+++ carrental/core/views.py
@@ -7,6 +7,7 @@ from .models import Car, Customer, Store, Transaction
 from django import forms
 from django.template import Template, Context
 from .forms import TransactionsGetForm, CarRecommendForm
+from .helpers import apply_filters
 from datetime import datetime, timedelta, time
 
 
@@ -121,7 +122,8 @@ class TransactionListView(generic.ListView):
     def get_queryset(self):
         """ Returns a list of transactions """
         return Transaction.objects.order_by('id')
-    
+
+# Recommend Car form 
 def recommend_car(request: HttpRequest):
     form = CarRecommendForm(request.GET)
     cars = list(Car.cars.all())
@@ -129,50 +131,3 @@ def recommend_car(request: HttpRequest):
     if (len(list(request.GET.values())) == 0):
         return render(request, 'extra/car-recommend.html', { 'form': form })    
     return render(request, 'extra/car-recommend.html', { 'form': form, 'recommended_cars': cars })
-
-def apply_filters(request: HttpRequest, cars: list):
-    cars = filter_cars(cars, "make", request.GET.get("make"))
-    cars = filter_cars(cars, "model", request.GET.get("model"))
-    cars = filter_cars(cars, "series_year", request.GET.get("series_year"))
-    cars = filter_cars(cars, "enginesize", request.GET.get("engine_size"))
-    cars = filter_cars(cars, "fuelsystem", request.GET.get("fuel_system"))
-    cars = filter_cars(cars, "power", request.GET.get("power"))
-    cars = filter_cars(cars, "seats", request.GET.get("seats"))
-    cars = filter_cars(cars, "bodyType", request.GET.get("body_type"))
-    cars = filter_cars(cars, "drive", request.GET.get("drive"))
-    cars = filter_cars(cars, "wheelbase", request.GET.get("wheelbase"))
-    return cars
-
-def filter_cars(collection: list, field: str, value: str):
-    if (value == '' or value is None):
-        return collection
-    values = value.split(" ")
-    if value.__contains__("<"):
-        check_type = "lt"
-        values.remove("<")
-    elif value.__contains__("BETWEEN"):
-        check_type = "between"
-        values.remove("BETWEEN")
-        values.remove("AND")
-    elif value.__contains__(">"):
-        check_type = "gt"
-        values.remove(">")
-    else:
-        check_type = "eq"
-    
-    for item in collection:
-        if not check_val(check_type, getattr(item, field), values):
-            collection.remove(item)
-    return collection
-
-def check_val(check_type: str, value, constraints: list):
-    if not constraints[0] or check_type == 'between' and not constraints[1]:
-        return True
-    elif (check_type == "between"):
-        return (int(value) > int(constraints[0]) and int(value) < int(constraints[1]))
-    elif (check_type == "lt"):
-        return int(value) < int(constraints[0])
-    elif (check_type == "gt"):
-        return int(value) > int(constraints[0])
-    else:
-        return str(value) == str(constraints[0])
