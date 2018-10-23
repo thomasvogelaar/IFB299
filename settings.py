c9545513a8b3516df4c5e65f4bd1f8b4d823b4be
Thomas Vogelaar <thomas@vogelaar.com.au>
2018-10-24 03:18:30 +1000

    Other database stuff for heroku.

--- settings.py
+++ settings.py
@@ -28,6 +28,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+
+DATABASES = { 'default' : dj_database_url.config()}
+
 
 # Application definition
 
