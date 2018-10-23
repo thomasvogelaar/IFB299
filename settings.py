6cd044e74c3d555f9bb76add47c5f59a9d20ce42
Thomas Vogelaar <thomas@vogelaar.com.au>
2018-10-24 03:14:58 +1000

    Trying to remove database related stuff.

--- settings.py
+++ settings.py
@@ -78,18 +78,6 @@ WSGI_APPLICATION = 'wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'carrental',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': '127.0.0.1',
-        'PORT': '3306'
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
